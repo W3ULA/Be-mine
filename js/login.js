8ea7bfc258e504b1500ffff660c331c36daca432
Олексій Паращин <7olexiy7@gmail.com>
2025-04-22 19:28:44 +0200

    first commit

---
+++ js/login.js
@@ -0,0 +1,37 @@
+// Показ/приховування пароля
+document.getElementById("show-password").addEventListener("change", function () {
+    const passwordField = document.getElementById("password");
+    passwordField.type = this.checked ? "text" : "password";
+});
+
+// При завантаженні сторінки, задаємо правильний тип
+window.addEventListener('load', function() {
+    const passwordField = document.getElementById("password");
+    const showPasswordCheckbox = document.getElementById("show-password");
+
+    // Якщо чекбокс увімкнений, змінюємо тип на "text"
+    if (showPasswordCheckbox.checked) {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+});
+// Обробка логіну
+document.getElementById("login-button").addEventListener("click", function (event) {
+    event.preventDefault(); // не перезавантажувати сторінку
+
+    const username = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+
+    // Проста перевірка
+    if (username === "Oleksii" && password === "govno") {
+        // Редірект
+        window.location.href = "main.html"; // зміни на свій файл
+    } else {
+        // Показати повідомлення про помилку
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "Invalid username or password";
+        errorMessage.style.color = "red";
+
+    }
+});
